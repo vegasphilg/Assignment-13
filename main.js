41e8239b3713636321c9698aa6215c9fac930510
Kristie White <insanelynocturnal@gmail.com>
2015-10-27 17:39:49 -0700

    5:39

--- main.js
+++ main.js
@@ -3,25 +3,20 @@ $(document).ready(function(){
 var formData = 'http://json-data.herokuapp.com/forms';
 
 $.ajax({
-  url: formURL,
-  dataType: 'json',
-  method: 'get'
-  
-}).then (function (formData) {
-	fa-user: formData.fa-user,
-	user-first-name:formData.user-first-name,
-	user-last-name: formData.user-last-name
-
+  url: formData,
+  method: "get",
+  dataType: "json"
+  }).then (function (formData) {
     var fetchedData = {
-        data: formData
-    };
-});
+    formData: formData
+};
+
     
 var formTemplate = $("#formTemplate").text();
-var newArrayhtml = Mustache.render(formTemplate , fetchedData);
-$("#main").html(newArrayhtml),
+var newArrayhtml = Mustache.render(formTemplate, fetchedData);
+$("#main").html(newArrayhtml);
 
     
-    
+});
   
 });//Closes Document Ready
